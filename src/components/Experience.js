f4429abe9a97ee9886c6b4fc59b18a5ec3df248c
Thomas <thomasbilhaut8@gmail.com>
2025-02-22 23:24:02 +0100

    detail expereince

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,8 +6,8 @@ const Experience = () => {
         <h2> Mes experience </h2>
 
         <ul>
-            <li> 2024-2025 : Stage chez Nemera au service IT, 6 semaines : 
-           <p> Document word, refait en site web alliant html, css et js </p> </li>
+            <li> 2024-2025 : Stage chez Nemera au service IT, 6 semaines :  </li>
+           <p> Document word, refait en site web alliant html, css et js </p>
             <li> 2023-2024 et 2024-2025 : emploie saisonnier sur machine d’assemblage
             5 mois en 5.8 chez nemera  </li>
         </ul>
